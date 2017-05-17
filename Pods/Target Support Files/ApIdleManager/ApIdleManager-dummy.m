10fd9d98bb351eaaaacbbc220780292652ce0ea8
Edwin Finch <contact@edwinfinch.com>
2017-05-18 01:43:32 +0800

    (Accidental double commit)

--- Pods/Target Support Files/ApIdleManager/ApIdleManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ApIdleManager : NSObject
-@end
-@implementation PodsDummy_ApIdleManager
-@end
--- Pods/Target Support Files/SecureNSUserDefaults/SecureNSUserDefaults-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SecureNSUserDefaults : NSObject
-@end
-@implementation PodsDummy_SecureNSUserDefaults
-@end
