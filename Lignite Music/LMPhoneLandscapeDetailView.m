621ee7bd70adfe81aa8d9acb9fcaefdc3001aaa2
Edwin Finch <contact@edwinfinch.com>
2017-06-10 15:45:53 +0800

    A bunch of improvements to the landscape view

--- Lignite Music/LMPhoneLandscapeDetailView.m
+++ Lignite Music/LMPhoneLandscapeDetailView.m
@@ -9,9 +9,12 @@
 #import <PureLayout/PureLayout.h>
 
 #import "LMPhoneLandscapeDetailView.h"
+#import "LMTiledAlbumCoverView.h"
+#import "YIInnerShadowView.h"
 #import "LMControlBarView.h"
 #import "LMTriangleView.h"
 #import "LMBigListEntry.h"
+#import "LMMusicPlayer.h"
 #import "LMDetailView.h"
 #import "LMColour.h"
 
@@ -38,6 +41,11 @@
 @property LMTriangleView *controlBarTriangle;
 
 /**
+ The control bar's inner shadow view.
+ */
+@property YIInnerShadowView *controlBarInnerShadowView;
+
+/**
  The actual control bar.
  */
 @property LMControlBarView *controlBar;
@@ -46,6 +54,11 @@
  The actual detail view for displaying shit.
  */
 @property LMDetailView *detailView;
+
+/**
+ The music player.
+ */
+@property LMMusicPlayer *musicPlayer;
 
 @end
 
@@ -123,25 +136,136 @@
 }
 
 - (id)contentSubviewForBigListEntry:(LMBigListEntry*)bigListEntry {
-	UIView *testview = [UIView newAutoLayoutView];
-	testview.backgroundColor = [UIColor blueColor];
-	return testview;
+	LMMusicTrackCollection *collection = self.musicTrackCollection;
+	
+	if(bigListEntry.contentView){
+		switch(self.musicType){
+			case LMMusicTypeComposers:
+			case LMMusicTypeArtists: {
+				UIImageView *imageView = bigListEntry.contentView;
+				UIImage *artistImage = [collection.representativeItem artistImage];
+				imageView.image = artistImage;
+				return imageView;
+			}
+			case LMMusicTypeAlbums:
+			case LMMusicTypeCompilations:
+			case LMMusicTypeGenres:
+			case LMMusicTypePlaylists: {
+				//No need for prep since we're just gonna prep once
+				LMTiledAlbumCoverView *tiledAlbumCover = bigListEntry.contentView;
+				tiledAlbumCover.musicCollection = collection;
+				return tiledAlbumCover;
+			}
+			default: {
+				NSLog(@"Windows fucking error!");
+				return nil;
+			}
+		}
+	}
+	else{
+		switch(self.musicType){
+			case LMMusicTypeComposers:
+			case LMMusicTypeArtists: {
+				UIImageView *imageView = [UIImageView newAutoLayoutView];
+				//			imageView.image = [[self.musicTrackCollections objectAtIndex:bigListEntry.collectionIndex].representativeItem artistImage];
+				imageView.contentMode = UIViewContentModeScaleAspectFit;
+				imageView.layer.shadowColor = [UIColor blackColor].CGColor;
+				imageView.layer.shadowRadius = WINDOW_FRAME.size.width/45;
+				imageView.layer.shadowOffset = CGSizeMake(0, imageView.layer.shadowRadius/2);
+				imageView.layer.shadowOpacity = 0.25f;
+				UIImage *artistImage = [collection.representativeItem artistImage];
+				imageView.image = artistImage;
+				return imageView;
+			}
+			case LMMusicTypeAlbums:
+			case LMMusicTypeCompilations:
+			case LMMusicTypeGenres:
+			case LMMusicTypePlaylists: {
+				//No need for prep since we're just gonna prep once
+				LMTiledAlbumCoverView *tiledAlbumCover = [LMTiledAlbumCoverView newAutoLayoutView];
+				tiledAlbumCover.musicCollection = collection;
+				return tiledAlbumCover;
+			}
+			default: {
+				NSLog(@"Windows fucking error!");
+				return nil;
+			}
+		}
+	}
 }
 
 - (float)contentSubviewFactorial:(BOOL)height forBigListEntry:(LMBigListEntry *)bigListEntry {
-	return 1.0;
+	return height ? 0.1 : 1.0;
 }
 
 - (NSString*)titleForInfoView:(LMCollectionInfoView*)infoView {
-	return @"Title";
+	LMMusicTrackCollection *collection = self.musicTrackCollection;
+	
+	switch(self.musicType){
+		case LMMusicTypeGenres: {
+			return collection.representativeItem.genre ? collection.representativeItem.genre : NSLocalizedString(@"UnknownGenre", nil);
+		}
+		case LMMusicTypeCompilations:{
+			return [collection titleForMusicType:LMMusicTypeCompilations];
+		}
+		case LMMusicTypePlaylists:{
+			return [collection titleForMusicType:LMMusicTypePlaylists];
+		}
+		case LMMusicTypeAlbums: {
+			return collection.representativeItem.albumTitle ? collection.representativeItem.albumTitle : NSLocalizedString(@"UnknownAlbum", nil);
+		}
+		case LMMusicTypeArtists: {
+			return collection.representativeItem.artist ? collection.representativeItem.artist : NSLocalizedString(@"UnknownArtist", nil);
+		}
+		case LMMusicTypeComposers: {
+			return collection.representativeItem.composer ? collection.representativeItem.composer : NSLocalizedString(@"UnknownComposer", nil);
+		}
+		default: {
+			return nil;
+		}
+	}
 }
 
 - (NSString*)leftTextForInfoView:(LMCollectionInfoView*)infoView {
-	return @"Left text";
+	LMMusicTrackCollection *collection = self.musicTrackCollection;
+	
+	switch(self.musicType){
+		case LMMusicTypeComposers:
+		case LMMusicTypeArtists: {
+			BOOL usingSpecificTrackCollections = (self.musicType != LMMusicTypePlaylists
+												  && self.musicType != LMMusicTypeCompilations
+												  && self.musicType != LMMusicTypeAlbums);
+			
+			if(usingSpecificTrackCollections){
+				//Fixes for compilations
+				NSUInteger albums = [self.musicPlayer collectionsForRepresentativeTrack:collection.representativeItem
+																		   forMusicType:self.musicType].count;
+				return [NSString stringWithFormat:@"%lu %@", (unsigned long)albums, NSLocalizedString(albums == 1 ? @"AlbumInline" : @"AlbumsInline", nil)];
+			}
+			else{
+				return [NSString stringWithFormat:@"%lu %@", (unsigned long)collection.numberOfAlbums, NSLocalizedString(collection.numberOfAlbums == 1 ? @"AlbumInline" : @"AlbumsInline", nil)];
+			}
+		}
+		case LMMusicTypeGenres:
+		case LMMusicTypePlaylists:
+		{
+			return [NSString stringWithFormat:@"%ld %@", (unsigned long)collection.trackCount, NSLocalizedString(collection.trackCount == 1 ? @"Song" : @"Songs", nil)];
+		}
+		case LMMusicTypeCompilations:
+		case LMMusicTypeAlbums: {
+			if(collection.variousArtists){
+				return NSLocalizedString(@"Various", nil);
+			}
+			return collection.representativeItem.artist ? collection.representativeItem.artist : NSLocalizedString(@"UnknownArtist", nil);
+		}
+		default: {
+			return nil;
+		}
+	}
 }
 
 - (NSString*)rightTextForInfoView:(LMCollectionInfoView*)infoView {
-	return @"Right test";
+	return nil;
 }
 
 - (UIImage*)centerImageForInfoView:(LMCollectionInfoView*)infoView {
@@ -156,16 +280,16 @@
 	if(!self.didLayoutConstraints){
 		self.didLayoutConstraints = YES;
 		
-		self.backgroundColor = [UIColor orangeColor];
+		self.backgroundColor = [UIColor whiteColor];
 		
 		
 		self.sidebarBackgroundView = [UIView newAutoLayoutView];
 		self.sidebarBackgroundView.backgroundColor = [UIColor whiteColor];
 		[self addSubview:self.sidebarBackgroundView];
 		
-		[self.sidebarBackgroundView autoPinEdgeToSuperviewEdge:ALEdgeTop];
 		[self.sidebarBackgroundView autoPinEdgeToSuperviewEdge:ALEdgeLeading];
-		[self.sidebarBackgroundView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
+		[self.sidebarBackgroundView autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
+		[self.sidebarBackgroundView autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:self withMultiplier:(9.5/10.0)];
 		[self.sidebarBackgroundView autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:self withMultiplier:(1.0/4.75)];
 		
 		
@@ -173,7 +297,6 @@
 		self.collectionInfoBigListEntry.infoDelegate = self;
 		self.collectionInfoBigListEntry.entryDelegate = self;
 		self.collectionInfoBigListEntry.collectionIndex = 0;
-		self.collectionInfoBigListEntry.backgroundColor = [UIColor orangeColor];
 		[self.sidebarBackgroundView addSubview:self.collectionInfoBigListEntry];
 		
 		[self.collectionInfoBigListEntry autoPinEdgeToSuperviewEdge:ALEdgeTop];
@@ -184,6 +307,18 @@
 		[self.collectionInfoBigListEntry setup];
 		
 		
+		self.controlBarTriangle = [LMTriangleView newAutoLayoutView];
+		self.controlBarTriangle.maskDirection = LMTriangleMaskDirectionUpwards;
+		self.controlBarTriangle.clipsToBounds = NO;
+		self.controlBarTriangle.triangleColour = [LMColour verticalControlBarGrayColour];
+		[self.sidebarBackgroundView addSubview:self.controlBarTriangle];
+		
+		[self.controlBarTriangle autoAlignAxisToSuperviewAxis:ALAxisVertical];
+		[self.controlBarTriangle autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.collectionInfoBigListEntry];
+		[self.controlBarTriangle autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:self.collectionInfoBigListEntry withMultiplier:(1.0/4.5)];
+		[self.controlBarTriangle autoMatchDimension:ALDimensionHeight toDimension:ALDimensionWidth ofView:self.controlBarTriangle withMultiplier:(5.0/8.0)];
+		
+		
 		self.controlBar = [LMControlBarView newAutoLayoutView];
 		self.controlBar.delegate = self;
 		self.controlBar.clipsToBounds = NO;
@@ -192,18 +327,14 @@
 		[self.controlBar autoPinEdgeToSuperviewEdge:ALEdgeBottom];
 		[self.controlBar autoAlignAxisToSuperviewAxis:ALAxisVertical];
 		[self.controlBar autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:self.collectionInfoBigListEntry];
-		[self.controlBar autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.collectionInfoBigListEntry];
+		[self.controlBar autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.controlBarTriangle];
 		
 		
-		self.controlBarTriangle = [LMTriangleView newAutoLayoutView];
-		self.controlBarTriangle.maskDirection = LMTriangleMaskDirectionUpwards;
-		self.controlBarTriangle.clipsToBounds = NO;
-		[self.controlBar addSubview:self.controlBarTriangle];
+		self.controlBarInnerShadowView = [YIInnerShadowView newAutoLayoutView];
+		self.controlBarInnerShadowView.shadowMask = YIInnerShadowMaskAll;
+		[self.controlBar addSubview:self.controlBarInnerShadowView];
 		
-		[self.controlBarTriangle autoAlignAxisToSuperviewAxis:ALAxisVertical];
-		[self.controlBarTriangle autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:self.controlBar];
-		[self.controlBarTriangle autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:self.controlBar withMultiplier:(1.0/5.0)];
-		[self.controlBarTriangle autoMatchDimension:ALDimensionHeight toDimension:ALDimensionWidth ofView:self.controlBarTriangle withMultiplier:(3.0/4.0)];
+		[self.controlBarInnerShadowView autoPinEdgesToSuperviewEdges];
 		
 		
 		self.detailView = [[LMDetailView alloc] initWithMusicTrackCollection:self.musicTrackCollection musicType:self.musicType];
