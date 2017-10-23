d06e565aa1d8d87593c2075cd39be6b28ee53193
Edwin Finch <contact@edwinfinch.com>
2017-10-23 10:47:38 -0400

    Add "add songs" button

--- Lignite Music/LMPlaylistEditorViewController.m
+++ Lignite Music/LMPlaylistEditorViewController.m
@@ -24,13 +24,21 @@
  */
 @property UITextField *titleTextField;
 
+/**
+ The add songs button which will be tapped by the user to add new songs.
+ */
+@property UIView *addSongsButtonView;
+
 @end
 
 @implementation LMPlaylistEditorViewController
 
-- (void)loadView {
-	self.view = [UIView new];
-	self.view.backgroundColor = [UIColor whiteColor];
+- (void)addSongsButtonTapped {
+	NSLog(@"Add songs...");
+}
+
+- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event {
+	[self.titleTextField resignFirstResponder];
 }
 
 - (void)viewDidLoad {
@@ -64,6 +72,56 @@
 	[textFieldLineView autoPinEdge:ALEdgeLeading toEdge:ALEdgeLeading ofView:self.titleTextField];
 	[textFieldLineView autoPinEdge:ALEdgeTrailing toEdge:ALEdgeTrailing ofView:self.titleTextField];
 	[textFieldLineView autoSetDimension:ALDimensionHeight toSize:1.0f];
+	
+	
+	self.addSongsButtonView = [UIView newAutoLayoutView];
+	self.addSongsButtonView.backgroundColor = [LMColour ligniteRedColour];
+	self.addSongsButtonView.layer.cornerRadius = 8.0f;
+	self.addSongsButtonView.layer.masksToBounds = YES;
+	[self.view addSubview:self.addSongsButtonView];
+	
+	[self.addSongsButtonView autoPinEdge:ALEdgeLeading toEdge:ALEdgeLeading ofView:self.titleTextField];
+	[self.addSongsButtonView autoPinEdge:ALEdgeTrailing toEdge:ALEdgeTrailing ofView:self.titleTextField];
+	[self.addSongsButtonView autoPinEdge:ALEdgeBottom toEdge:ALEdgeBottom ofView:self.imagePickerView];
+	[self.addSongsButtonView autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:self.imagePickerView withMultiplier:(2.0/3.0)];
+	
+	UITapGestureRecognizer *addSongsButtonTapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(addSongsButtonTapped)];
+	[self.addSongsButtonView addGestureRecognizer:addSongsButtonTapGestureRecognizer];
+	
+	NSString *text = NSLocalizedString(@"AddSongs", nil);
+	UIImage *icon = [LMAppIcon imageForIcon:LMIconAdd];
+	
+	UIView *backgroundView = [UIView newAutoLayoutView];
+	[self.addSongsButtonView addSubview:backgroundView];
+	
+	[backgroundView autoMatchDimension:ALDimensionHeight toDimension:ALDimensionHeight ofView:self.addSongsButtonView withMultiplier:(1.0/4.0)];
+	[backgroundView autoCenterInSuperview];
+	
+	UIImageView *iconView = [UIImageView newAutoLayoutView];
+	iconView.image = icon;
+	iconView.contentMode = UIViewContentModeScaleAspectFit;
+	[backgroundView addSubview:iconView];
+	
+	[iconView autoPinEdgeToSuperviewEdge:ALEdgeLeading];
+	[iconView autoPinEdgeToSuperviewEdge:ALEdgeTop];
+	[iconView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
+	[iconView autoMatchDimension:ALDimensionWidth toDimension:ALDimensionHeight ofView:backgroundView];
+	
+	UILabel *labelView = [UILabel newAutoLayoutView];
+	labelView.text = text;
+	labelView.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
+	labelView.textColor = [UIColor whiteColor];
+	[backgroundView addSubview:labelView];
+	
+	[labelView autoPinEdge:ALEdgeLeading toEdge:ALEdgeTrailing ofView:iconView withOffset:12.0f];
+	[labelView autoPinEdgeToSuperviewEdge:ALEdgeTop];
+	[labelView autoPinEdgeToSuperviewEdge:ALEdgeTrailing];
+	[labelView autoPinEdgeToSuperviewEdge:ALEdgeBottom];
+}
+
+- (void)loadView {
+	self.view = [UIView new];
+	self.view.backgroundColor = [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning {
